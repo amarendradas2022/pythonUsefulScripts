0dabf64c4b47e04615199241e9c3c36d2a69e75b
amarendradas2022 <98778924+amarendradas2022@users.noreply.github.com>
2023-09-27 15:55:20 -0400

    Update getParquetSchemaInDatabricks.py
    Removed unwanted libraries

--- getParquetSchemaInDatabricks.py
+++ getParquetSchemaInDatabricks.py
@@ -5,12 +5,6 @@ import logging
 import yaml
 import pyspark
 import glob
-from pyspark.sql import SQLContext,Row
-from pyspark.sql.functions import *
-from pyspark.sql.types import StringType
-from pyspark.sql import SparkSession,DataFrame
-from pyspark.sql.utils import AnalysisException
-from pyspark.sql.functions import lit,col,lower,trim,coalesce,upper,when
 
 table_list=['fire_rating_record','homeowners_rating_rec']
 for i in table_list:
